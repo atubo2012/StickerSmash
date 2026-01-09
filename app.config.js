7dbe3f02bef43ddcbff6543835cce13258629d5e
Sean <sean.wang.cn@gmail.com>
2026-01-08 23:48:04 -0800

    add environment variables for build mode

---
+++ app.config.js
@@ -0,0 +1,35 @@
+export default ({ config }) => ({
+    ...config,
+    name: getAppName(),
+    android: {
+        ...config.android,
+        package: getUniqueIdentifier(),
+    },
+});
+
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+
+const getUniqueIdentifier = () => {
+    if (IS_DEV) {
+        return 'com.seanexpo2026.stickersmash.dev';
+    }
+
+    if (IS_PREVIEW) {
+        return 'com.seanexpo2026.stickersmash.preview';
+    }
+
+    return 'com.seanexpo2026.stickersmash';
+};
+
+const getAppName = () => {
+    if (IS_DEV) {
+        return 'StickerSmash (Dev)';
+    }
+
+    if (IS_PREVIEW) {
+        return 'StickerSmash (Preview)';
+    }
+
+    return 'StickerSmash: Emoji Stickers';
+};
